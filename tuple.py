6e5c776e0fcd20cd006bc783808b1a9fba83959d
aldoe21 <89310022+aldoe21@users.noreply.github.com>
2021-12-21 15:26:19 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,28 @@
+#tuple concept as i known for
+a=0
+b=0
+c=0
+t=(1,2,3)
+
+#type 1 unpacking
+a,b,c=t
+print(a,b,c)
+print("\n")
+
+#type 2 unpacking - I
+t=(1,2,3,4)
+a,*b,c=t
+print(a,b,c)
+print("\n")
+
+#type 2 unpacking - II
+d=0
+t=(1,2,3,4,5)
+a,*b,c,d=t
+print(a,b,c,d)
+print("\n")
+
+#swapping, becoming a tuple and gets unpacked
+a,b=b,a
+print(a,b)
+
